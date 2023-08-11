06987ade61856535dcbd29329ee50c65820ad17e
LionMara <lionofmara@gmail.com>
2023-08-11 07:23:49 +0300

    some tweaks

--- models/user.py
+++ models/user.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+""" users class gives the details of created users"""
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
